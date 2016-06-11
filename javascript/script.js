dd6e5007a9d3c2c6feb40d5dd73f078fde581e1e
Max Austin <maxozzy22@gmail.com>
2016-06-11 16:21:09 -0700

    Added a bunch of images and almost finished orginization

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,26 @@
 $(function(){
-  $('header li').hover(function(){
+  $('li#content-selector').hover(function(){
     $(this).toggleClass('hover', 200);
   });
 
   $('header li').click(function(){
-    $('#section-title').html("<h2>"+$(this).attr('name')+"</h2>");
-  });
-  $('html #content-selector').click(function(){
     $('main').removeClass('hide');
-    $('header').animate({height: '50vh'});
+    $('header').animate({height: '20vh'});
+    $('header ul').css('flex-direction', 'row').css('justify-content', 'stretch');
+  });
+
+  $('li#content-selector').click(function(){
+    var pagecontent = $('#'+$(this).attr('name'));
     $(this).addClass('active').siblings().removeClass('active');
-    $('#'+$(this).attr('name')).show(500).siblings().filter('section').hide(500);
+    pagecontent.siblings().filter('section').fadeOut(500, function(){
+      pagecontent.fadeIn(500);
+    });
+  });
+
+  $('#tabs li').click(function(){
+    var step = $(this).parent().parent().parent().parent().attr('id');
+    var navbarSelction = $(this).parent().parent().attr('id');
+    var tabSelection = $(this).attr('name');
+    $('#'+$(this).attr('name')).load('pages/'+step+'/'+navbarSelction+'/'+tabSelection+'.html');
   });
 });
