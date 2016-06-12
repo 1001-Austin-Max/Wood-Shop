2f242b227d02d06b0d0c801106c6f8a76448fa44
Max Austin <maxozzy22@gmail.com>
2016-06-11 21:48:38 -0700

    Worked out a final organization for imagine
    need to integrate that into design and build

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('li#content-selector').hover(function(){
+  $('*#content-selector').hover(function(){
     $(this).toggleClass('hover', 200);
   });
 
@@ -9,18 +9,36 @@ $(function(){
     $('header ul').css('flex-direction', 'row').css('justify-content', 'stretch');
   });
 
-  $('li#content-selector').click(function(){
-    var pagecontent = $('#'+$(this).attr('name'));
-    $(this).addClass('active').siblings().removeClass('active');
-    pagecontent.siblings().filter('section').fadeOut(500, function(){
-      pagecontent.fadeIn(500);
-    });
+  $('*#content-selector').click(function(){
+    if($(this).nodeName != "div"){
+      var pagecontent = $('#'+$(this).attr('name'));
+      $(this).addClass('active').siblings().removeClass('active');
+      pagecontent.siblings().filter('section').fadeOut(500, function(){
+        pagecontent.fadeIn(500);
+      });
+    }
+  });
+
+  $('div#content-selector').click(function(){
+    if($(this).parent().attr('id') == 'dropdown'){
+      var step = $(this).parents('section[id=imagine]').attr('id');
+      var navbarSelection = $(this).parent().parent().parent().parent().attr('id');
+      var tabSelection = $(this).attr('name');
+      console.log('section #' + $(this).attr('name'));
+      $('section #' + $(this).attr('name')).load('pages/'+step+'/'+navbarSelection+'/'+tabSelection+'.html');
+    }
   });
 
   $('#tabs li').click(function(){
-    var step = $(this).parent().parent().parent().parent().attr('id');
-    var navbarSelction = $(this).parent().parent().attr('id');
-    var tabSelection = $(this).attr('name');
-    $('#'+$(this).attr('name')).load('pages/'+step+'/'+navbarSelction+'/'+tabSelection+'.html');
+    $(this).children().slideDown(200);
+  });
+
+  $('#tabs li #dropdown').mouseleave(function(){
+    if(!$(this).parent().is(':hover')){
+      $(this).slideUp(200);
+    }
+  });
+  $('#tabs li').mouseleave(function(){
+    $(this).children().slideUp(200);
   });
 });
